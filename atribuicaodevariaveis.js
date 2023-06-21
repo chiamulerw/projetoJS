b49a96bf8d8b8244776c2c5ebd2a1e73c6bbcd26
cvhenx <127249227+cvhenx@users.noreply.github.com>
2023-06-21 17:40:34 -0300

    Add files via upload

---
+++ atribuicaodevariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com atribuição");
+
+const idade =16;
+const primeiroNome ="Ana";
+const sobrenome ="Chiamulera";
+
+console.log(primeiroNome, sobrenome);
+// outra forma de dar espaco é usar (nome + " " + sobrenome)
+console.log(`Meu nome é ${primeiroNome + " " + sobrenome}`);
+
+let contador = 0;
+contador = contador + 1;
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
