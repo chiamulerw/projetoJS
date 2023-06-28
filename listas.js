f98779f5a8665cff7fe53628a13a18457ff4f38c
cvhenx <127249227+cvhenx@users.noreply.github.com>
2023-06-28 09:04:05 -0300

    Add files via upload

--- listas.js
+++ listas.js
@@ -3,6 +3,7 @@ console.log(`Trabalhando com listas`);
 // const saoPaulo = `São Paulo`;
 // const rioDeJaneiro = `Rio de Janeiro`;
 
+
 const listaDeDestinos = new Array(
     `Salvador`,
     `São Paulo`,
