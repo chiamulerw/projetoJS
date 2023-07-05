d62ce46c8604d39b4e9a9034d26bd02c3f734679
cvhenx <127249227+cvhenx@users.noreply.github.com>
2023-07-05 09:56:38 -0300

    Add files via upload

--- loops.js
+++ loops.js
@@ -17,7 +17,26 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true ;
 
 let contador = 0;
+let destinoExiste = false;
 while(contador<3){
-    console.log(listaDeDestinos[contador]);
+    if(listaDeDestinos[contador] == destino){
+        destinoExiste = true;
+        break;
+    }
     contador += 1;
 }
+console.log("Destino existe; ", destinoExiste);
+
+if(podeComprar && destinoExiste){
+    console.log("Boa Viagem!")
+}else{
+    console.log("Desculpe, tivemos um erro!")
+}
+
+for(let cont = 0;contador < 3; cont ++){
+    if(listaDeDestinos[contador] == destino){
+        destinoExiste = true;
+        break;
+    }
+}
+
